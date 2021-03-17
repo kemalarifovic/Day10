0d95f80a3320d3611d13e0fc198f0e396550ce2c
Kemal Arifovic <gigalab@gmail.com>
2021-03-17 10:57:15 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+var myPet = {
+  species: "doberman",
+  name: "tiny",
+  legs: 4,
+  friends: ["spitz", "johny"],
+};
+function myFunction(myObj) {
+  return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
