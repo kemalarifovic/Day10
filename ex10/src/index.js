6595a716931dcb1aa3863a42054e417b239949f8
Kemal Arifovic <gigalab@gmail.com>
2021-03-17 14:37:21 +0100

    Complex Objects

---
+++ ex10/src/index.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  var myMusic = {
+    001: {
+      artist: "Billy Joel",
+      title: "Piano Man",
+      release_year: 1973,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP",
+      },
+      gold: true,
+    },
+    002: {
+      artist: "Frank Sinatra",
+      title: "My way",
+      release_year: 1969,
+      formats: {
+        1: "CD",
+        2: "8T",
+        3: "LP",
+      },
+      gold: true,
+    },
+  };
+  return myMusic;
+}
+myFunction();
+console.log(myFunction()[2]);
+module.exports = myFunction;
